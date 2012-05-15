23436dd3862d75b2e9747e8c8e4edd650399e057
ncrohn <nick@meltmedia.com>
2012-05-14 17:03:53 -0700

    Updates to mca

--- mca.js
+++ mca.js
@@ -1,10 +1,40 @@
-module.exports = function(path, app, logger) {
-  var pkg = require(path);
+var path = require('path');
 
-  logger.info("      name :".blue, pkg.name.grey);
-  logger.info("   version :".blue, pkg.version.grey);
-  logger.info("started at :".blue, Date().grey);
-  //logger.info("   on port :".blue, app.server.address().port.grey);
-  logger.info("   in mode :".blue, process.env.NODE_ENV.grey);
+module.exports = function(logger, props) {
+  var pkg = require(path.join(process.cwd(), 'package.json')),
+      labelWidth = 12,
+      defaults = {
+        name: pkg.name,
+        version: pkg.version,
+        started: Date(),
+        mode: process.env.NODE_ENV,
+        platform: (process.platform + " " + process.arch),
+        node: process.version,
+        process: process.pid,
+        running: process.mainModule.filename
+      };
+
+  process.title = pkg.name;
+
+  function logInfo(key, value) {
+    var label = "",
+        spaces = labelWidth - key.length;
+
+    for(var i=0;i<spaces;i++) {
+      label += " ";
+    }
+
+    label = label + key + " :";
+
+    logger.info(label.blue, value.toString().grey);
+  }
+
+  for(var k in defaults) {
+    logInfo(k, defaults[k]);
+  }
+
+  for(var k in props) {
+    logInfo(k, props[k]);
+  }
 
 };
